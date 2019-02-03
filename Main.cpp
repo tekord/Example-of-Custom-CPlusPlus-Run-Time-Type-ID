c4a807ec529503224e37bd07a86a1da336af11ca
tekord <tekord@gmail.com>
2019-02-03 18:40:35 +0400

    Added more usage examples in Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,12 @@
 
 #include "RTTI.h"
 
-struct FooObject : public TypeIdProvider {
+struct NonRttiObject {
+	int x;
+	int y;
+};
+
+struct FooObject : public ITypeIdProvider {
 	RTTI_HAS_TYPE_ID
 
 	int x, y;
@@ -17,22 +22,122 @@ struct BarObject : public FooObject {
 	float n[4];
 };
 
-RTTI_DEFINE_TYPE_ID(FooObject, "FooObject");
-RTTI_DEFINE_TYPE_ID(BarObject, "BarObject");
+struct SomeObject : public FooObject {
+	RTTI_HAS_TYPE_ID
+};
+
+RTTI_DEFINE_TYPE_ID(FooObject);
+RTTI_DEFINE_TYPE_ID(BarObject);
+RTTI_DEFINE_CUSTOM_TYPE_ID(SomeObject, "CustomObject");
 
 void dumpTypeId(const RttiTypeId *instance) {
+	if (instance == nullptr) {
+		std::cout << "No type provided" << std::endl;
+
+		return;
+	}
+
 	std::cout << "TYPE " << instance->name << ":" << std::endl
 		<< "   SIZE: " << instance->size << std::endl
 		<< "   RUNTIME ID: " << instance->runtimeId << std::endl;
 }
 
+class MethodBasedVisitor {
+public:
+	void visit(ITypeIdProvider &object) {
+		auto type = object.getTypeId();
+
+#define _CASE(cppClass) \
+	else if (type == cppClass::typeId()) \
+		visit(reinterpret_cast<cppClass&>(object))
+
+		if (0)
+			;
+		_CASE(FooObject);
+		_CASE(BarObject);
+		else {
+			std::cout << "Fallback for " << object.getTypeId()->name << std::endl;
+		}
+
+#undef _CASE
+	}
+
+	void visit(FooObject node) {
+		std::cout << "Special visit for FooObject" << std::endl;
+	}
+
+	void visit(BarObject node) {
+		std::cout << "Special visit for BarObject" << std::endl;
+	}
+};
+
+class LambdaBasedVisitor {
+public:
+	void visit(ITypeIdProvider &object) {
+
+		auto type = object.getTypeId();
+
+#define _CASE(cppClass, lambda) \
+	else if (type == cppClass::typeId()) \
+		lambda(reinterpret_cast<cppClass&>(object))
+
+		if (0)
+			;
+		_CASE(FooObject, [&](FooObject &node) {
+			std::cout << "Special visit for FooObject" << std::endl;
+		});
+		_CASE(SomeObject, [&](SomeObject &node) {
+			std::cout << "Special visit for SomeObject" << std::endl;
+		});
+		else {
+			std::cout << "Fallback for " << object.getTypeId()->name << std::endl;
+		}
+
+#undef _CASE
+	}
+};
+
 int main()
 {
-	dumpTypeId(FooObject::typeId());
-	dumpTypeId(BarObject::typeId());
+	std::cout << "__cplusplus: " << __cplusplus << std::endl;
 
 	assert(FooObject::typeId() == FooObject::typeId());
 	assert(FooObject::typeId() != BarObject::typeId());
 	assert(FooObject::typeId()->name == "FooObject");
 	assert(BarObject::typeId()->name == "BarObject");
+
+	std::vector<ITypeIdProvider *> objects;
+	objects.push_back(new BarObject());
+	objects.push_back(new FooObject());
+	objects.push_back(new SomeObject());
+	objects.push_back(new BarObject());
+	objects.push_back(new FooObject());
+	objects.push_back(new SomeObject());
+
+	dumpTypeId(FooObject::typeId());
+	dumpTypeId(RttiTypeId::get(objects[0]));
+	dumpTypeId(RttiTypeId::get<BarObject>());
+	dumpTypeId(RTTI_GET_TYPE(SomeObject));
+
+#if __cplusplus >= 201703L
+	dumpTypeId(RttiTypeId::get<NonRttiObject>()); // expected nullptr
+#endif
+
+	dumpTypeId(RTTI_GET_TYPE_UNSAFE(BarObject));
+
+	MethodBasedVisitor visitor1;
+
+	std::cout << std::endl << "MethodBasedVisitor:" << std::endl;
+
+	for (auto &i : objects) {
+		visitor1.visit(*i);
+	}
+
+	LambdaBasedVisitor visitor2;
+
+	std::cout << std::endl << "LambdaBasedVisitor:" << std::endl;
+
+	for (auto &i : objects) {
+		visitor2.visit(*i);
+	}
 }
